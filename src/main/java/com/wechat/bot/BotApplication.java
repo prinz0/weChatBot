3eb9f56a7bf5d6292f07739a558ef05d33fb9cac
lichangxing <897385237@qq.com>
2019-08-22 10:25:54 +0800

    no message

--- src/main/java/com/wechat/bot/BotApplication.java
+++ src/main/java/com/wechat/bot/BotApplication.java
@@ -2,12 +2,21 @@ package com.wechat.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BotApplication {
+public class BotApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BotApplication.class);
     }
 
 }
